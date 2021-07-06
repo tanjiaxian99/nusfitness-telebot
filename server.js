4e077beef6ac10c80a10e2632726bffa6e30d5f0
Tan Jia Xian <e0543981@u.nus.edu>
2021-07-07 00:58:07 +0800

    C:/Program Files/Git/start checks that user is logged in to telegram login widget

--- server.js
+++ server.js
@@ -1,4 +1,44 @@
-const Telegraf = require("telegraf");
+const { Telegraf, Markup } = require("telegraf");
+const fetch = require("node-fetch");
 require("dotenv").config();
 
 const bot = new Telegraf(process.env.TOKEN);
+
+bot.start((ctx) => {
+  const url = `${
+    process.env.NODE_ENV === "production"
+      ? "http://local.nusfitness.com:5000/"
+      : "https://salty-reaches-24995.herokuapp.com/"
+  }telegram/isLoggedIn`;
+
+  fetch(url, {
+    method: "post",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      chatId: ctx.update.message.chat.id,
+    }),
+    credentials: "include",
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      if (res.success) {
+        ctx.reply(
+          "What would you like to do today?",
+          Markup.inlineKeyboard([
+            Markup.button.callback("Booking", "Booking"),
+            Markup.button.callback("Dashboard", "Dashboard"),
+          ])
+        );
+      } else {
+        ctx.reply(
+          "You are currently not logged in to @NUSFitness_Bot. Please login using the NUSFitness website."
+        );
+      }
+    });
+});
+
+bot.launch();
+
+// Enable graceful stop
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
